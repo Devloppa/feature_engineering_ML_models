df84acb90c7905555d90263d227262124c1d354a
Marat Matourin <marat.matorin@gmail.com>
2022-12-28 10:18:15 +0100

    Set index to false and replace dataset

--- preprocess/datasets.py
+++ preprocess/datasets.py
@@ -1,13 +1,12 @@
 import numpy as np
 import pandas as pd
-import os
 
 
 def titanic():
     data = pd.read_csv("https://www.openml.org/data/get_csv/16826755/phpMYEkMl")
     data = data.replace("?", np.nan)
     data["cabin"] = data["cabin"].apply(get_first_cabin)
-    data.to_csv("datasets/titanic.csv")
+    data.to_csv("datasets/titanic.csv", index=False)
 
 
 def get_first_cabin(row):
